e8ec4fe1c43c23632a40f78a715ec4cdd78d2a68
SamsonDeVol <devols@oregonstate.edu>
2021-10-20 16:30:23 -0700

    fix whitespace

--- cploration/c04/parser.c
+++ cploration/c04/parser.c
@@ -9,15 +9,12 @@ char *strip(char *s){
   char s_new[strlen(s)+1];
   int i = 0;
   for (char *s2 = s; *s2; s2++){
-
     if (*s2=='/' && *(s+1)=='/'){
-
       break;
     }
     else if (!isspace(*s2)){
       s_new[i++] = *s2;
     }
-    
   }
   s_new[i] = '\0';
   strcpy(s, s_new);
@@ -29,7 +26,6 @@ void parse(FILE * file){
   while (fgets(line, sizeof(line), file)){
     strip(line); 
     if(!*line){
-      
       continue;
     }
     printf("%s\n", line);
