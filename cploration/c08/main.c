49b097f946a622a1611c86a78a794e3466244dd0
SamsonDeVol <devols@oregonstate.edu>
2021-11-29 00:32:21 -0800

    correct exit program arguements

--- cploration/c08/main.c
+++ cploration/c08/main.c
@@ -4,6 +4,7 @@
 int main(int argc, const char *argv[]){
 
 if(argc != 2){
+    printf("here");
     // incorrect number of arguments
     exit_program(EXIT_INCORRECT_ARGUMENTS, argv[0]);
 }
@@ -11,7 +12,7 @@ if(argc != 2){
 FILE *fin = fopen(argv[1], "r");
  
 if(fin == NULL){
-    exit_program(EXIT_INCORRECT_ARGUMENTS, argv[0]);
+    exit_program(EXIT_CANNOT_OPEN_FILE, argv[1]);
 }
 parse(fin);
 fclose(fin);
